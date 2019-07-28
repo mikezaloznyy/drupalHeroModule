19adf4ae4d5c12c6fe8f7b467b7256a6f16637eb
Mike Zaloznyy <myz@myz-mbp.centurylink.net>
2019-07-28 02:46:30 -0700

    add table as renderable array

--- module_hero/src/Plugin/Block/HeroBlock.php
+++ module_hero/src/Plugin/Block/HeroBlock.php
@@ -17,9 +17,38 @@ class HeroBlock extends BlockBase {
      * {@inheritdoc}
      */
     public function build(){
-        return [
-            '#type' => 'markup',
-            '#markup' => $this->t('The output of our superheroes block.'),
+        $heroes = [
+            ['hero_name' => 'Hulk', 'real_name' => 'David Banner'],
+            ['hero_name' => 'Thor', 'real_name' => 'Thor Odinson'],
+            ['hero_name' => 'Iron Man', 'real_name' => 'Tony Stark'],
+            ['hero_name' => 'Luke Cage', 'real_name' => 'Carl Lucas'],
+            ['hero_name' => 'Black Widow', 'real_name' => 'Natalia Romanova'],
+            ['hero_name' => 'Daredevil', 'real_name' => 'Matthew Murdock'],
+            ['hero_name' => 'Captain America', 'real_name' => 'Steven Rogers'],
+            ['hero_name' => 'Wolverine', 'real_name' => 'James Howlett']
+          ];      
+
+        $table = [
+        '#type' => 'table',
+        '#header' => [
+                $this->t('Hero name'),
+                $this->t('Real name'),
+            ]
         ];
+        
+        foreach($heroes as $hero) {
+            $table[] = [
+              'hero_name' => [
+                '#type' => 'markup',
+                '#markup' => $hero['hero_name'],
+              ],
+              'real_name' => [
+                '#type' => 'markup',
+                '#markup' => $hero['real_name'],
+              ],
+            ];
+          }
+      
+          return $table;
     }
 }
