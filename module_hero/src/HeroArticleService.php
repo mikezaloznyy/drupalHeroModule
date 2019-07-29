eabc3549832fc8acc09b5074ebdfcf5d24375b8b
Mike Zaloznyy <mike.zaloznyy@gmail.com>
2019-07-28 20:12:03 -0700

    inject entity query and entity manager services

--- module_hero/src/HeroArticleService.php
+++ module_hero/src/HeroArticleService.php
@@ -2,17 +2,28 @@
 
 namespace Drupal\module_hero;
 
+use Drupal\Core\Entity\Query\QueryFactory;
+use  Drupal\Core\Entity\EntityManager;
+
 /**
  * Our hero article service class.
  */
 class HeroArticleService {
 
+  private $entityQuery;
+  private $entityManager;
+
+  public function __construct(QueryFactory $entityQuery, EntityManager $entityManager) {
+    $this->entityQuery = $entityQuery;
+    $this->entityManager = $entityManager;
+  }
+
   /**
    * Methood for getting Articles, regarding heroes.
    */
   public function getHeroArticles() {
-    $articles = ['Hulk is green!', 'Flash is red!'];
+    $articleNids = $this->entityQuery->get('node')->condition('type', 'article')->execute();
 
-    return $articles;
+    return $this->entityManager->getStorage('node')->loadMultiple($articleNids);
   }
 }
