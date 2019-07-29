7edd1b9d7aa71ded0e58bce934ad495a07b5e7e3
Mike Zaloznyy <mike.zaloznyy@gmail.com>
2019-07-28 20:33:47 -0700

    inject service into the controller

--- module_hero/src/Controller/HeroController.php
+++ module_hero/src/Controller/HeroController.php
@@ -3,12 +3,28 @@
 namespace Drupal\module_hero\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\module_hero\HeroArticleService;
 
 /**
  * This is our hero controller
  */
 class HeroController extends ControllerBase {
+    private $articleHeroService;
+
+    public static function create(ContainerInterface $container) {
+        return new static(
+            $container->get('module_hero.hero_articles')
+        );
+    }
+
+    public function __construct(HeroArticleService $articleHeroService) {
+        $this->articleHeroService = $articleHeroService;
+    }
+
     public function heroList() {
+        kint($this->articleHeroService->getHeroArticles()); die();
+        
         $heroes = [
             ['name' => 'Hulk'],
             ['name' => 'Thor'],
